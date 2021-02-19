32f9816ced0390d85a72e6ed4188612b0bca95bd
bk <>
2021-02-19 13:03:57 +0700

    update README.md

--- src/index.js
+++ src/index.js
@@ -1,26 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 
 const registerServiceWorker = () => {
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker
       .register("./firebase-messaging-sw.js")
-      .then(function (registration) {
-        console.log("Registration successful, scope is:", registration.scope);
+      .then(function () {
+        console.log("Service worker registration successful");
       })
       .catch(function (err) {
-        console.log("Service worker registration failed, error:", err);
+        console.log("Service worker registration failed: ", err);
       });
   }
 };
 
+// register service worker
 registerServiceWorker();
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
